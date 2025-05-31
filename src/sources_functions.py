3a0148a93c8b4dff2ca7568dd8d362e01a26cb09
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-05-31 00:03:37 -0300

    ✨ feat: making request to access articles

--- src/sources_functions.py
+++ src/sources_functions.py
@@ -1,8 +1,16 @@
 import random
 import string
-from typing import List
+import requests
+from bs4 import BeautifulSoup
 
-def select_random_source(sources_list: List) -> string:
+def select_random_source(sources_list: list) -> string:
     selected_url = random.choice(sources_list)
     
     return selected_url
+
+def access_articles_on_source(url: string) -> list:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "xml")
+    articles = soup.find_all("item")
+    
+    return articles
