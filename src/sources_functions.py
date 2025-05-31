29d3ac1c9e8858cda98debf9eddaf7091ec472f2
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-05-31 00:06:07 -0300

    ✨ feat: parsing articles to extract main informations

--- src/sources_functions.py
+++ src/sources_functions.py
@@ -14,3 +14,16 @@ def access_articles_on_source(url: string) -> list:
     articles = soup.find_all("item")
     
     return articles
+
+def parse_article_information(article: list) -> list:
+    informations = []
+
+    article_title = article.find("title").text
+    article_description = article.find("description").text
+    article_link = article.find("link").text
+
+    informations.append(article_title)
+    informations.append(article_description)
+    informations.append(article_link)
+
+    return informations
