d0c5614aeed4e1adff894707b457b82b89f372a1
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-05-30 23:22:10 -0300

    ✨ feat: create list of sources

---
+++ src/sources_list.py
@@ -0,0 +1,5 @@
+[
+    "https://www.technologyreview.com/topic/artificial-intelligence/feed",
+    "https://www.quantamagazine.org/computer-science/feed/",
+    "https://rss.app/feeds/WQuquvEuDFhvJ1qh.xml"
+]
