05ed5aadeaf68e8e14db9a79b35610e812c793bf
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-05-30 23:48:55 -0300

    ⚡️ feat: add try...catch block on already_shared function

--- src/database_functions.py
+++ src/database_functions.py
@@ -1,12 +1,14 @@
 import sqlite3
 
+database_path = "./database/articles.db"
+
 connection = sqlite3.connect("./database/articles.db")
 cursor = connection.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOTE EXISTS articles (
-   id INTERGER PRIMARY KEY,
-    title TEXT NOT NULL;              
+   id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL            
 )
 ''')
 
@@ -14,12 +16,13 @@ connection.commit()
 connection.close()
 
 def already_shared(article_title: str) -> bool:
-    connection = sqlite3.connect("./database/articles.db")
-    cursor = connection.cursor()
+    """Verify if the selected article was previosly shared"""
+    try:
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+        cursor.execute('SELECT 1 FROM articles WHERE title = ?', (article_title))
+        result = cursor.fetchone()
 
-    cursor.execute('SELECT 1 FROM articles WHERE title = ?', (article_title))
-    result = cursor.fetchone()
-
-    connection.close()
-
-    return result is not None
+        return result is not None
+    except sqlite3.Error as exception:
+        raise exception
