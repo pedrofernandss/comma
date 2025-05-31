68d99f2ac58912be49b6ae5891be8b7cc02659b4
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-05-31 00:00:15 -0300

    ♻️ refactor: database initialization to function

--- src/database_functions.py
+++ src/database_functions.py
@@ -1,19 +1,32 @@
+import os
 import sqlite3
 
 database_path = "./database/articles.db"
 
-connection = sqlite3.connect("./database/articles.db")
-cursor = connection.cursor()
+def initialize_database() -> None:
+    """Create database"""
 
-cursor.execute('''
-CREATE TABLE IF NOTE EXISTS articles (
-   id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL            
-)
-''')
+    db_directiory = os.path.dirname(database_path)
 
-connection.commit()
-connection.close()
+    if db_directiory and not os.path.exists(db_directiory):
+        os.makedirs(db_directiory)
+
+    try:
+        connection = sqlite3.connect("./database/articles.db")
+        cursor = connection.cursor()
+
+        cursor.execute('''
+        CREATE TABLE IF NOTE EXISTS articles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL            
+        )
+        ''')
+
+        connection.commit()
+        
+    except sqlite3.Error as CreationDatabaseError:
+        raise CreationDatabaseError
+
 
 def save_data(article_title: str) -> None:
     """Save a new article on the database"""
