65b9d3722eced88d37730d45d149c1d9a4610942
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-10-05 21:16:49 -0300

    ✨ feat: função orquestradora end-to-end

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import * as dotenv from 'dotenv';
+import rssSources from './config/article_sources.json';
+import { selectRandomSource } from './content/selector';
+import { fetchLatestArticle } from './content/fetcher';
+import { sendWhatsappMessage } from './whatsapp/index';
+
+dotenv.config();
+
+async function runBot() {
+  try {
+    const source = selectRandomSource(rssSources);
+
+    if (!source) {
+      console.log('No source selected. Exiting');
+      return;
+    }
+
+    const article = await fetchLatestArticle(source);
+    if (!article) {
+      console.log('No new article found from source. Exiting.');
+    }
+
+    await sendWhatsappMessage(article!);
+    console.log('Bot finished successfully.');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+runBot();
