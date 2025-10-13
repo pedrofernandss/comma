e8de1551db209cf31944b132c0d8fe4031b233e8
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-10-12 23:23:02 -0300

    ✨ feat: realização de 10 tentativas de encontrar artigo novo

--- src/scheduler/index.ts
+++ src/scheduler/index.ts
@@ -1,34 +1,38 @@
-import rssSources from './config/article_sources.json';
-import { selectRandomSource } from './content/selector';
-import { fetchLatestArticle } from './content/fetcher';
-import { sendWhatsappMessage } from './whatsapp/index';
-import { MssqlStorageService } from '@/storage';
+import rssSources from '../config/article_sources.json';
+import { selectRandomSource } from '../content/selector';
+import { fetchLatestArticle } from '../content/fetcher';
+import { sendWhatsappMessage } from '../whatsapp/index';
+import { MssqlStorageService } from '../storage';
 
 export async function runDailyUpdate() {
   try {
     const storageService = new MssqlStorageService();
+    const max_attempts = 10;
 
-    const source = selectRandomSource(rssSources);
+    for (let i = 0; i < max_attempts; i++) {
+      const source = selectRandomSource(rssSources);
 
-    if (!source) {
-      console.log('No source selected. Exiting');
-      return;
+      if (!source) {
+        console.log('No source selected. Exiting');
+        continue;
+      }
+
+      const article = await fetchLatestArticle(source);
+      if (!article) {
+        console.log('No new article found from source. Exiting.');
+        continue;
+      }
+
+      const isNew = !(await storageService.hasBeenSent(article.link!));
+
+      if (isNew) {
+        await sendWhatsappMessage(article!);
+        await storageService.saveAsSent(article.link!);
+        return;
+      } else {
+        console.log(`Article "${article.title}" has already been sent. Trying another one...`);
+      }
     }
-
-    const article = await fetchLatestArticle(source);
-    if (!article) {
-      console.log('No new article found from source. Exiting.');
-    }
-
-    if(await storageService.hasBeenSent(article.link!)) {
-      
-    } else {
-      await storageService.saveAsSent(article.link)
-      await sendWhatsappMessage(article!);
-    }
-
-    
-
     console.log('Bot finished successfully.');
   } catch (error) {
     console.error(error);
