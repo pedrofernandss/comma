1ab36d98b068353718c8281b116b9b4969e59367
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-12-29 02:09:30 -0300

    add project from spring initilizr

---
+++ src/test/java/com/comma/newsbot/CommaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comma.newsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
