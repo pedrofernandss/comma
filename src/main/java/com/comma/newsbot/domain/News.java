33652c1098b0a8a1074261a52df66be57117b43c
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-12-29 12:35:00 -0300

    feat: add news entity class

---
+++ src/main/java/com/comma/newsbot/domain/News.java
@@ -0,0 +1,30 @@
+package com.comma.newsbot.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "news")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String title;
+
+    @Column(nullable = false, length = 850, unique = true)
+    private String url;
+
+    private LocalDateTime publishedAt;
+
+    private LocalDateTime processedAt;
+}
