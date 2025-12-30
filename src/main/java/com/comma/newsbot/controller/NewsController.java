c413f2736adf509aa68a1ab7bc1738a72cda2b9c
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-12-29 23:35:11 -0300

    feat: controller for test functions in postman

---
+++ src/main/java/com/comma/newsbot/controller/NewsController.java
@@ -0,0 +1,23 @@
+package com.comma.newsbot.controller;
+
+import com.comma.newsbot.service.NewsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/news")
+public class NewsController {
+
+    private final NewsService newsService;
+
+    public NewsController(NewsService newsService){
+        this.newsService = newsService;
+    }
+
+    @PostMapping("/trigger")
+    public String trigger(){
+        newsService.getRandomFeedUrl();
+        return "Sorteio executado! Verifique o console do IntelliJ.";
+    }
+}
